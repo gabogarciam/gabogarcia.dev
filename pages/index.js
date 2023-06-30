5abd13249772909e87c9dbbcd8d7633c1d7c014a
Gabriel Garcia <gabogarciadev@gmail.com>
2023-06-30 18:17:19 +0200

    feat: :art: created firts structure
    created firts structure of the page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
-const Page = () => {
-    return <div> Taaada! starting project</div>
+import { Container, Box, Heading, useColorModeValue } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        textAlign="center"
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m Gabriel, developer making awesome things for awesome
+        people!
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Gabriel García
+          </Heading>
+          <p>Climber / Developer / Photographer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
-export default Page
+export default Home
