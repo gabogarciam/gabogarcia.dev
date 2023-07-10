eba4ae601b247b6bce542302cff9ac1e42acc2ae
Gabriel Garcia <gabogarciadev@gmail.com>
2023-07-11 01:03:18 +0200

    refactor: :lipstick: update animation
    update animation toggle button

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -10,10 +10,18 @@ const ThemeToggleButton = () => {
       <motion.div
         style={{ display: 'inline-block' }}
         key={useColorModeValue('light', 'dark')}
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        exit={{ y: 20, opacity: 0 }}
-        transition={{ duration: 0.2 }}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            type: 'spring',
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001
+          }
+        }}
       >
         <IconButton
           aria-label="Toggle theme"
