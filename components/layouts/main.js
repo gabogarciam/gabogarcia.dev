f0923bea8179fc3b9f0f4ed53753f197ba67b0d4
Gabriel Garcia <gabogarciadev@gmail.com>
2023-07-11 01:01:39 +0200

    feat: :sparkles: added footer, meta and favico
    added footer, meta and favico

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,17 +1,22 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Gabriel García - Homepage</title>
+        <meta name="description" content="Gabriel homepage" />
+        <meta name="author" content="Gabriel García" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <title>Gabriel García</title>
       </Head>
       <Navbar path={router.route} />
       <Container maxW="container.md" pt={14}>
         {children}
+        <Footer />
       </Container>
     </Box>
   )
