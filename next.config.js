ef658a0e418481b388ac404178afbfdc49798f49
Gabriel Garcia <gabogarciadev@gmail.com>
2023-07-24 23:56:39 +0200

    feat: :sparkles: contentlayer configuration
    contentlayer configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
+const { withContentlayer } = require("next-contentlayer");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //   appDir: true,
-    // },
-    reactStrictMode: true,
-    swcMinify: true
-  }
-  
-module.exports = nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = withContentlayer(nextConfig);
